4fdd6ed77bdafff3d0adbe13d0b2670d3442430b
Javier Pérez Gorricho <japerego@unirioja.loc>
2021-02-25 18:50:23 +0100

    Initial commit

---
+++ MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
